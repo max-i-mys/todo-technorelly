6bb31343101d1edc3cc13127e2eec244eec008fa
max-i-mys <maximys6205@gmail.com>
2021-11-04 18:05:44 +0200

    added LocalStorage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,17 @@
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { initialTodo } from '../slicers/todosSlice'
 import TodoItem from './TodoItem'
 
 export default function TodoList() {
+	const dispatch = useDispatch()
 	const todos = useSelector(state => state.todos.todos)
+	useEffect(() => {
+		if (!localStorage.getItem('todos')) {
+			localStorage.setItem('todos', JSON.stringify([]))
+		} else {
+			dispatch(initialTodo(JSON.parse(localStorage.getItem('todos'))))
+		}
+	}, [])
 	return <>{todos ? todos.map(todo => <TodoItem key={todo.id} todo={todo} />) : <h3>List is empty</h3>}</>
 }
--- src/slicers/todosSlice.js
+++ src/slicers/todosSlice.js
@@ -6,12 +6,16 @@ const todosSlice = createSlice({
 		todos: [],
 	},
 	reducers: {
+		initialTodo: (state, action) => {
+			state.todos = [...action.payload]
+		},
 		addTodo: (state, action) => {
 			state.todos.push(action.payload)
+			localStorage.setItem('todos', JSON.stringify(state.todos))
 		},
 	},
 })
 
 export default todosSlice.reducer
 
-export const { addTodo } = todosSlice.actions
+export const { initialTodo, addTodo } = todosSlice.actions
