7b48d461af55c83777aa552082c5515099709abe
max-i-mys <maximys6205@gmail.com>
2021-11-05 17:38:11 +0200

    finished to update todos status

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,12 @@
-import { Card, CloseButton } from 'react-bootstrap'
+import { Card, CloseButton, Form } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { deleteTodo, updateTodo } from '../slicers/todosSlice'
 import { formatterDate } from '../utils/constants'
 export default function TodoItem({ todo }) {
 	const dispatch = useDispatch()
-	function handlerUpdate(e) {
-		e.preventDefault()
-		dispatch(updateTodo({ id: todo.id }))
-	}
 	return (
 		<>
-			<Card className="todo" border={todo.status ? 'success' : 'dark'} onContextMenu={handlerUpdate}>
+			<Card className={`todo ${todo.status ? '' : 'finished'}`} border={todo.status ? 'success' : 'danger'}>
 				<Card.Body>
 					<Card.Title className="d-flex justify-content-between">
 						{todo.title}
@@ -19,6 +15,13 @@ export default function TodoItem({ todo }) {
 					<Card.Subtitle className="mb-2 text-muted">{formatterDate.format(todo.id)}</Card.Subtitle>
 					<Card.Text>{todo.description}</Card.Text>
 				</Card.Body>
+				<Form.Check
+					onClick={() => dispatch(updateTodo({ id: todo.id }))}
+					type="checkbox"
+					label="finished"
+					className="m-2"
+					defaultChecked={todo.status ? false : true}
+				/>
 			</Card>
 		</>
 	)
